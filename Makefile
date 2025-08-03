68e9d4c4d34f8fb554a055c8476043bb5dfcf2cf
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-08-03 13:44:54 +0000

    chore: コンポーネント関連のmakeコマンド変更

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 gen-story:
-	@bash scripts/gen-story.sh $(name)
+	@bash scripts/gen-story.sh $(word 1,$(MAKECMDGOALS)) $(word 2,$(MAKECMDGOALS))
 
 gen-component:
-	@bash scripts/gen-component.sh $(name)
+	@bash scripts/gen-component.sh $(word 1,$(MAKECMDGOALS)) $(word 2,$(MAKECMDGOALS))
+
+%:
+	@:
--- scripts/template/StoryTemplate.stories.ts
+++ scripts/template/StoryTemplate.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 import __COMPONENT__ from './index';
 
 const meta: Meta<typeof __COMPONENT__> = {
-  title: 'Components/__COMPONENT__',
+  title: 'Components/__TYPE__/__COMPONENT__',
   component: __COMPONENT__,
   parameters: {
     layout: 'center',
