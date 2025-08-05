690651f3facfad350df9d3db3fead117397f2992
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-08-05 16:06:39 +0000

    add: 折りたたみコンポーネントを作成

---
+++ src/components/ui/Collapse/Item.tsx
@@ -0,0 +1,23 @@
+import type { ReactNode } from 'react';
+import { cn } from '../../../utils/cn';
+
+export type Props = {
+  children: ReactNode;
+  className?: string;
+  role: 'title' | 'content';
+};
+
+const Item: React.FC<Props> = ({ children, className = '', role }) => {
+  return (
+    <div
+      className={cn(
+        role === 'title' ? 'collapse-title' : 'collapse-content',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Item;
---
+++ src/components/ui/Collapse/index.tsx
@@ -0,0 +1,73 @@
+import type { ReactElement } from 'react';
+import { cn } from '../../../utils/cn';
+import type { ClassMap } from '../../../types/component';
+import type { Props as BaseItemProps } from './Item';
+import Item from './Item';
+
+type ItemProps = Omit<BaseItemProps, 'role'>;
+
+const TitleItem: React.FC<ItemProps> = ({ children, className = '' }) =>
+  Item({
+    children,
+    className,
+    role: 'title',
+  });
+
+const ContentItem: React.FC<ItemProps> = ({ children, className = '' }) =>
+  Item({
+    children,
+    className: className,
+    role: 'content',
+  });
+
+type Icon = 'arrow' | 'plus';
+
+const iconMap: ClassMap<Icon, 'collapse'> = {
+  arrow: 'collapse-arrow',
+  plus: 'collapse-plus',
+};
+
+type ForceState = 'open' | 'close';
+
+const forceStateMap: ClassMap<ForceState, 'collapse'> = {
+  open: 'collapse-open',
+  close: 'collapse-close',
+};
+
+type Props = {
+  children: ReactElement<ItemProps> | ReactElement<ItemProps>[];
+  icon: Icon;
+  name?: string;
+  className?: string;
+  forceState?: ForceState;
+};
+
+const Collapse: React.FC<Props> & {
+  Title: typeof TitleItem;
+  Content: typeof ContentItem;
+} = ({
+  children,
+  icon = 'arrow',
+  name = 'collapse',
+  forceState,
+  className = '',
+}) => {
+  return (
+    <div
+      className={cn(
+        'collapse w-full',
+        iconMap[icon],
+        forceState && forceStateMap[forceState],
+        className
+      )}
+    >
+      <input type="checkbox" name={name} />
+      {children}
+    </div>
+  );
+};
+
+Collapse.Title = TitleItem;
+Collapse.Content = ContentItem;
+
+export default Collapse;
