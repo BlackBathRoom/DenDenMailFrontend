4b1f73b61f1ec5c1b96b17754dd42bf05b52342f
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-09-27 19:37:42 +0000

    add: アプリ内で使用する型定義を追加

---
+++ src/types/index.ts
@@ -0,0 +1,58 @@
+type DateTime = {
+  year: number;
+  month: number;
+  day: number;
+  hour: number;
+  minute: number;
+};
+
+type MessageInfo = {
+  id: number;
+  subject: string;
+  senderAddress: string;
+  isRead: boolean;
+  receivedAt: DateTime;
+};
+
+type Attachment = {
+  id: number;
+  filename: string;
+  mimeType: string;
+  mimeSubtype: string;
+  sizeBytes: number;
+};
+
+type MessageDetail = {
+  messageInfo: MessageInfo;
+  textBody: string | null;
+  htmlBody: string | null;
+  attachments: Attachment[] | null;
+};
+
+type AddressInfo = {
+  id: number;
+  address: string;
+  displayName: string | null;
+};
+
+type VendorInfo = {
+  id: number;
+  name: string;
+  iconUrl: string;
+};
+
+type FolderInfo = {
+  id: number;
+  name: string;
+  messageCount: number;
+};
+
+export type {
+  AddressInfo,
+  Attachment,
+  DateTime,
+  FolderInfo,
+  MessageDetail,
+  MessageInfo,
+  VendorInfo,
+};
