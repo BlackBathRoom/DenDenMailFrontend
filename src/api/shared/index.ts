51ac0e134b7b6bc8284ce1380a19490c9deeddd1
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-09-27 19:41:33 +0000

    add: api配下で共有する処理、定義を追加
    - tanstack queryのエラー型をAxiosのエラー型に
    - BaseDTOを定義
    - axiosを用いたリクエスト用の関数を作成

---
+++ src/api/shared/index.ts
@@ -0,0 +1,71 @@
+import '@tanstack/react-query';
+import type {
+  AxiosError,
+  AxiosRequestConfig as _AxiosRequestConfig,
+} from 'axios';
+import axios from 'axios';
+
+declare module '@tanstack/react-query' {
+  interface Register {
+    defaultError: AxiosError;
+  }
+}
+
+interface BaseDTO {
+  id: number;
+}
+
+type AxiosRequestConfig = Omit<
+  _AxiosRequestConfig,
+  'url' | 'baseURL' | 'method'
+>;
+
+// TODO: いい感じの管理方法を考える
+const BASE_URL = 'http://localhost:8000';
+
+const axiosRequest = <T = unknown>(
+  endpoint: string,
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH',
+  config: AxiosRequestConfig = {}
+) => {
+  const conf: _AxiosRequestConfig = {
+    baseURL: `${BASE_URL}`,
+    url: `/api/${endpoint}`,
+    method,
+    ...config,
+  };
+
+  return axios<T>(conf);
+};
+
+const getRequest = async <T = unknown>(
+  endpoint: string,
+  config: AxiosRequestConfig = {}
+): Promise<T> => {
+  return await axiosRequest<T>(endpoint, 'GET', config).then((res) => res.data);
+};
+
+const postRequest = async <T>(
+  endpoint: string,
+  data: T,
+  config: AxiosRequestConfig = {}
+) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  return await axiosRequest(endpoint, 'POST', { data, headers, ...config });
+};
+
+const patchRequest = async <T>(
+  endpoint: string,
+  data: T,
+  config: AxiosRequestConfig = {}
+) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  return await axiosRequest(endpoint, 'PATCH', { data, headers, ...config });
+};
+
+export { getRequest, patchRequest, postRequest };
+export type { BaseDTO };
